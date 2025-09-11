d747fd2b9f0e5a41bac575361290f9ba8e00143f
Alik <al.humbatli@gmail.com>
2025-09-11 19:12:19 +0400

    hw2_2upd

--- lesson-02/02.js
+++ lesson-02/02.js
@@ -7,8 +7,9 @@
 * Для успешного прохождения тестов не меняйте названия переменных!
 */
 
-let dice1 // your code
-let dice2 // your code
+let dice1 = Math.floor(Math.random() * 6) + 1; // случайное число 1–6
+let dice2 = Math.floor(Math.random() * 6) + 1; // случайное число 1–6
+
 let isWinningDouble = dice1 === dice2
 console.log('Первый бросок: ' + dice1)
 console.log('Второй бросок: ' + dice2)
