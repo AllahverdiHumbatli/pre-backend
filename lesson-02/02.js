7906cbcad9277cd912d170c4b8fed1d84cd883af
Alik <al.humbatli@gmail.com>
2025-09-11 20:00:29 +0400

    hw_2_2_upd1

--- lesson-02/02.js
+++ lesson-02/02.js
@@ -9,8 +9,8 @@
 
 let dice1 = Math.floor(Math.random() * 6) + 1; // случайное число 1–6
 let dice2 = Math.floor(Math.random() * 6) + 1; // случайное число 1–6
+let isWinningDouble = dice1 === dice2 && dice1 > 3;
 
-let isWinningDouble = dice1 === dice2
 console.log('Первый бросок: ' + dice1)
 console.log('Второй бросок: ' + dice2)
 if (isWinningDouble && dice1>=3&&dice1<=6) {
